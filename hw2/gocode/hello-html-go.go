78972cf0134aa4d3de2f1530b8de5e83e679e521
Ajvait28 <adomasvait@gmail.com>
2026-01-30 16:46:01 -0800

    fixed hello-html.go

--- hw2/gocode/hello-html-go.go
+++ hw2/gocode/hello-html-go.go
@@ -8,11 +8,14 @@ import (
 )
 
 func main() {
+	// CGI programs write directly to stdout
 	w := os.Stdout
 
+	// HTTP header
 	fmt.Fprintln(w, "Content-Type: text/html; charset=utf-8")
 	fmt.Fprintln(w)
 
+	// Data
 	name := "Adomas Vaitkus"
 	language := "Go"
 	now := time.Now().Format("2006-01-02 15:04:05")
@@ -22,6 +25,7 @@ func main() {
 		ip = forwarded
 	}
 
+	// HTML output
 	fmt.Fprintln(w, `<!DOCTYPE html>
 <html>
 <head>
@@ -35,4 +39,8 @@ func main() {
 <p><strong>Name:</strong> `+name+`</p>
 <p><strong>Language:</strong> `+language+`</p>
 <p><strong>Date & Time:</strong> `+now+`</p>
-<p><st
+<p><strong>Your IP Address:</strong> `+ip+`</p>
+
+</body>
+</html>`)
+}
