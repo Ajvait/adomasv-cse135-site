bc7fa75a7df5a9c8afefbdc355f432c6b04576b2
Ajvait28 <adomasvait@gmail.com>
2026-03-11 16:22:45 -0700

    Completing second part of hw5

--- analytics/export_pdf.php
+++ analytics/export_pdf.php
@@ -1,15 +1,51 @@
 <?php
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
 
-require __DIR__ . "/vendor/autoload.php";
+require "auth.php";
+require "db.php";
 
-use Dompdf\Dompdf;
+require_once __DIR__ . "/vendor/dompdf/dompdf/dompdf_config.inc.php";
 
-$dompdf = new Dompdf();
+$dompdf = new DOMPDF();
 
-$html = "<h1>Test PDF</h1>";
+$query = "
+SELECT id, session_id, event_type, url, created_at
+FROM events
+ORDER BY created_at DESC
+LIMIT 100
+";
 
-$dompdf->loadHtml($html);
+$result = $conn->query($query);
+
+$html = "<h1>Analytics Report</h1>";
+$html .= "<table border='1' cellpadding='5'>";
+
+$html .= "<tr>
+<th>ID</th>
+<th>Session</th>
+<th>Event</th>
+<th>URL</th>
+<th>Time</th>
+</tr>";
+
+while($row = $result->fetch_assoc()){
+
+$html .= "<tr>";
+
+$html .= "<td>".$row["id"]."</td>";
+$html .= "<td>".$row["session_id"]."</td>";
+$html .= "<td>".$row["event_type"]."</td>";
+$html .= "<td>".$row["url"]."</td>";
+$html .= "<td>".$row["created_at"]."</td>";
+
+$html .= "</tr>";
+}
+
+$html .= "</table>";
+
+$dompdf->load_html($html);
+
 $dompdf->render();
-$dompdf->stream("test.pdf");
+
+$dompdf->stream("analytics_report.pdf");
+
+?>
