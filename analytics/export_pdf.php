c0633e4e9b43e1b6755485e1f6298ee3e845dbe1
Ajvait28 <adomasvait@gmail.com>
2026-03-11 16:29:48 -0700

    Completing second part of hw5

--- analytics/export_pdf.php
+++ analytics/export_pdf.php
@@ -3,11 +3,8 @@
 require "auth.php";
 require "db.php";
 
-require __DIR__ . "/vendor/autoload.php";
-
-use Dompdf\Dompdf;
-
-$dompdf = new Dompdf();
+header("Content-Type: application/octet-stream");
+header("Content-Disposition: attachment; filename=analytics_report.html");
 
 $query = "
 SELECT id, session_id, event_type, url, created_at
@@ -18,10 +15,11 @@ LIMIT 100
 
 $result = $conn->query($query);
 
-$html = "<h1>Analytics Report</h1>";
-$html .= "<table border='1' cellpadding='5'>";
+echo "<h1>Analytics Report</h1>";
 
-$html .= "<tr>
+echo "<table border='1' cellpadding='5'>";
+
+echo "<tr>
 <th>ID</th>
 <th>Session</th>
 <th>Event</th>
@@ -31,20 +29,15 @@ $html .= "<tr>
 
 while($row = $result->fetch_assoc()){
 
-$html .= "<tr>";
+echo "<tr>";
 
-$html .= "<td>".$row["id"]."</td>";
-$html .= "<td>".$row["session_id"]."</td>";
-$html .= "<td>".$row["event_type"]."</td>";
-$html .= "<td>".$row["url"]."</td>";
-$html .= "<td>".$row["created_at"]."</td>";
+echo "<td>".$row["id"]."</td>";
+echo "<td>".$row["session_id"]."</td>";
+echo "<td>".$row["event_type"]."</td>";
+echo "<td>".$row["url"]."</td>";
+echo "<td>".$row["created_at"]."</td>";
 
-$html .= "</tr>";
+echo "</tr>";
 }
 
-$html .= "</table>";
-
-$dompdf->loadHtml($html);
-$dompdf->setPaper("A4", "portrait");
-$dompdf->render();
-$dompdf->stream("analytics_report.pdf");
+echo "</table>";
